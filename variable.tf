10f9ba880b23ce12b26b135f0ef1825d82aa79a8
kumariseait <93650590+kumariseait@users.noreply.github.com>
2023-11-08 14:40:00 +0530

    variables of webserver

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "ec2_ami" {
+type = string
+default = "ami-02e94b011299ef128"
+}
+
+variable "inst_type" {
+type = string
+default = "t2.micro"
+}
+
+variable "inst_az" {
+type = string
+default = "ap-south-1a"
+}
+
